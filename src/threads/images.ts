47f7e9a81315c7a43f3c0ec1d63d365d32f6925c
wcyat <wcyat@wcyat.me>
2022-06-04 23:35:56 +0800

    fix image type

--- src/threads/images.ts
+++ src/threads/images.ts
@@ -1,11 +1,11 @@
 import { Axios } from "axios";
 import objToQuery from "../lib/objToQuery";
-import { Images } from "../types/images";
+import { Image } from "../types/image";
 
 export default function images(axios: Axios) {
     return (options: { threadId: number; commentId?: number }) => {
         const { threadId, commentId } = options;
         const query = objToQuery({ c: commentId });
-        return axios.get<Images>(`/thread/${threadId}/images${query ? "?" + query : ""}`);
+        return axios.get<Image[]>(`/thread/${threadId}/images${query ? "?" + query : ""}`);
     };
 }
