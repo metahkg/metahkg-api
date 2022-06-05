19c79b26d0f1ac69d728f0fca3f28e23d70068c9
wcyat <wcyat@wcyat.me>
2022-06-05 16:35:26 +0800

    either category id or thread id is required

--- dist/menu/search.js
+++ dist/menu/search.js
@@ -9,7 +9,7 @@ function default_1(axios) {
         const query = (0, objToQuery_1.default)({
             page,
             sort: sort && { Relevance: 0, Topic: 1, LastReply: 2, 0: 0, 1: 1, 2: 2 }[sort],
-            mode: mode && mode === "Title" ? 0 : 1,
+            mode: mode && { Title: 0, OP: 1, 0: 0, 1: 1 }[mode],
             q: searchQuery,
         });
         return axios.get(`/menu/search${query ? `?${query}` : ""}`);
--- src/menu/main.ts
+++ src/menu/main.ts
@@ -1,19 +1,27 @@
 import { Axios } from "axios";
 import objToQuery from "../lib/objToQuery";
 import { Summary } from "../types/thread/thread";
+import { RequireAtLeastOne } from "../types/xor";
 
 export default function (axios: Axios) {
-    return (options: {
-        // category id or bytid<thread id>
-        categoryId: number | string;
-        sort?: "Latest" | "Viral" | 0 | 1;
-        page?: number;
-    }) => {
-        const { categoryId, sort, page } = options;
+    return (
+        options: RequireAtLeastOne<
+            {
+                categoryId?: number;
+                threadId?: number;
+                sort?: "Latest" | "Viral" | 0 | 1;
+                page?: number;
+            },
+            "categoryId" | "threadId"
+        >
+    ) => {
+        const { categoryId, threadId, sort, page } = options;
         const query = objToQuery({
             sort: sort && { Latest: 0, Viral: 1, 0: 0, 1: 1 }[sort],
             page,
         });
-        return axios.get<Summary[]>(`/menu/${categoryId}${query ? `?${query}` : ""}`);
+        return axios.get<Summary[]>(
+            `/menu/${categoryId || `bytid${threadId}`}${query ? `?${query}` : ""}`
+        );
     };
 }
