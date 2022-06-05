d8a0a76afc0d67d48785c1e85e1084179fa6887f
wcyat <wcyat@wcyat.me>
2022-06-05 18:56:06 +0800

    fix

--- dist/lib/objToQuery.js
+++ dist/lib/objToQuery.js
@@ -1,7 +1,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 function objToQuery(obj) {
     return Object.keys(obj)
-        .filter((key) => obj[key])
+        .filter((key) => obj[key] !== undefined && obj[key] !== null)
         .map((key) => `${key}=${obj[key]}`)
         .join("&");
 }
---
+++ dist/threads/error.d.ts
@@ -0,0 +1,3 @@
+export interface ErrorMsg {
+    error: string;
+}
