eb841cd07a3d71f1c15db6d4391adce5a5c672c6
wcyat <wcyat@wcyat.me>
2022-06-05 14:10:30 +0800

    fix typo and allow using number for sort

--- src/menu/history.ts
+++ src/menu/history.ts
@@ -5,11 +5,14 @@ import { Summary } from "../types/thread/thread";
 export default function (axios: Axios) {
     return (options: {
         userId: number | "self";
-        sort?: "Topic" | "LastReply";
+        sort?: "Topic" | "LastReply" | 0 | 1;
         page?: number;
     }) => {
         const { userId, sort, page } = options;
-        const query = objToQuery({ sort: sort && sort === "Topic" ? 0 : 1, page });
+        const query = objToQuery({
+            sort: sort && { Topic: 0, LastReply: 1, 0: 0, 1: 1 }[sort],
+            page,
+        });
         return axios.get<Summary[]>(`/menu/history/${userId}${query ? `?${query}` : ""}`);
     };
 }
--- src/menu/main.ts
+++ src/menu/main.ts
@@ -5,11 +5,14 @@ import { Summary } from "../types/thread/thread";
 export default function (axios: Axios) {
     return (options: {
         categoryId: number;
-        sort?: "Latest" | "Viral";
+        sort?: "Latest" | "Viral" | 0 | 1;
         page?: number;
     }) => {
         const { categoryId, sort, page } = options;
-        const query = objToQuery({ sort: sort && sort === "Latest" ? 0 : 1, page });
+        const query = objToQuery({
+            sort: sort && { Latest: 0, Viral: 1, 0: 0, 1: 1 }[sort],
+            page,
+        });
         return axios.get<Summary[]>(`/menu/${categoryId}${query ? `?${query}` : ""}`);
     };
 }
