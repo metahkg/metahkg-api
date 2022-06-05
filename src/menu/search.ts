57a332518d6fe5f15674d13f9357b1316f46dfb3
wcyat <wcyat@wcyat.me>
2022-06-05 15:35:39 +0800

    allow smode as number

--- src/menu/search.ts
+++ src/menu/search.ts
@@ -7,14 +7,14 @@ export default function (axios: Axios) {
         page?: number;
         searchQuery: string;
         sort?: "Relevance" | "Topic" | "LastReply" | 0 | 1 | 2;
-        mode?: "Title" | "OP";
+        mode?: "Title" | "OP" | 0 | 1;
     }) => {
         const { page, searchQuery, sort, mode } = options;
         const query = objToQuery({
             page,
             sort:
                 sort && { Relevance: 0, Topic: 1, LastReply: 2, 0: 0, 1: 1, 2: 2 }[sort],
-            mode: mode && mode === "Title" ? 0 : 1,
+            mode: mode && { Title: 0, OP: 1, 0: 0, 1: 1 }[mode],
             q: searchQuery,
         });
         return axios.get<Summary[]>(`/menu/search${query ? `?${query}` : ""}`);
