a0041ac3f89222f301f5b7f2a9c10b20a0ec4779
wcyat <wcyat@wcyat.me>
2022-06-05 13:43:46 +0800

    fix profile type

--- src/profile/userProfile.ts
+++ src/profile/userProfile.ts
@@ -1,8 +1,9 @@
 import { Axios } from "axios";
+import { Profile } from "../types/user";
 
 export default function (axios: Axios) {
     return (options: { userId: number | "self"; nameonly?: boolean }) => {
         const { userId, nameonly } = options;
-        return axios.get<string>(`/profile/${userId}${nameonly ? "?nameonly=1" : ""}`);
+        return axios.get<Profile>(`/profile/${userId}${nameonly ? "?nameonly=1" : ""}`);
     };
 }
--- src/types/user.ts
+++ src/types/user.ts
@@ -5,5 +5,12 @@ export interface User {
     role: userRole;
 }
 
+export type Profile =  User & {
+    /** date string */
+    createdAt: string;
+    /** number of threads */
+    count: 14;
+}
+
 export type userSex = "M" | "F";
 export type userRole = "admin" | "user";
