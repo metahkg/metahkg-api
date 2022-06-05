34019ab1f376572ebe737a7d745cc7d87d376a5e
wcyat <wcyat@wcyat.me>
2022-06-05 14:52:17 +0800

    fix menu

--- src/menu/index.ts
+++ src/menu/index.ts
@@ -1,13 +1,13 @@
 import { Axios } from "axios";
-import profile from "../profile/userProfile";
-import threads from "../threads";
+import history from "./history";
+import threads from "./threads";
 import main from "./main";
 import search from "./search";
 
 export default function menu(axios: Axios) {
     return {
         threads: threads(axios),
-        profile: profile(axios),
+        profile: history(axios),
         /** the main menu (get threads by category) */
         main: main(axios),
         search: search(axios),
