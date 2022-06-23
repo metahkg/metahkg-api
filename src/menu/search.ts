4bc5699d80024788ad9c2c8fc8217264df740aa8
wcyat <wcyat@wcyat.me>
2022-06-23 15:42:10 +0800

    v2.1.0

--- src/menu/search.ts
+++ src/menu/search.ts
@@ -6,14 +6,14 @@ export default function (axios: Axios) {
     return (options: {
         page?: number;
         searchQuery: string;
-        sort?: "Relevance" | "Topic" | "LastReply" | 0 | 1 | 2;
+        sort?: "Relevance" | "Created" | "LastReply" | 0 | 1 | 2;
         mode?: "Title" | "OP" | 0 | 1;
     }) => {
         const { page, searchQuery, sort, mode } = options;
         const query = objToQuery({
             page,
             sort:
-                sort && { Relevance: 0, Topic: 1, LastReply: 2, 0: 0, 1: 1, 2: 2 }[sort],
+                sort && { Relevance: 0, Created: 1, LastReply: 2, 0: 0, 1: 1, 2: 2 }[sort],
             mode: mode && { Title: 0, OP: 1, 0: 0, 1: 1 }[mode],
             q: searchQuery,
         });
