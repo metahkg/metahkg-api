dc0c243930a8a4761ab62f61b210c51d243b8d8d
wcyat <wcyat@wcyat.me>
2022-06-25 16:41:28 +0800

    update

--- src/users/index.ts
+++ src/users/index.ts
@@ -1,7 +1,7 @@
 import { Axios } from "axios";
 import avatar from "./avatar";
 import block from "./block";
-import editprofile from "./rename";
+import rename from "./rename";
 import login from "./login";
 import register from "./register";
 import resend from "./resend";
@@ -14,7 +14,7 @@ export default function users(axios: Axios) {
     return {
         uploadAvatar: avatar(axios),
         block: block(axios),
-        editprofile: editprofile(axios),
+        rename: rename(axios),
         login: login(axios),
         register: register(axios),
         resend: resend(axios),
