e6493065554956d2d31763fe88bcb8f16a0e589d
wcyat <wcyat@wcyat.me>
2022-07-11 19:05:59 +0800

    fix blocklist

--- src/users/blocklist.ts
+++ src/users/blocklist.ts
@@ -2,7 +2,7 @@ import { Axios } from "axios";
 import { User } from "../types/user";
 
 export default function (axios: Axios) {
-    () => {
+    return () => {
         /** get blocked users */
         return axios.get<User[]>("/users/blocklist");
     };
