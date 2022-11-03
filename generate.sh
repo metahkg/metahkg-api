55c84e2eceef91a78aa4faeb0e025f1d009c2c7f
wcyat <wcyat@wcyat.me>
2022-11-03 17:40:40 +0800

    update generate.sh and gitlab ci

--- generate.sh
+++ generate.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 wget "https://gitlab.com/metahkg/metahkg-server/-/raw/$(git symbolic-ref --short HEAD)/openapi.yaml"
 version=$(cat openapi.yaml | grep version | awk -F: '{ print $2 }' | sed 's/[" ]//g' | sed 's/-dev//g')
 oldversion=$(cat package.json | grep version | awk -F: '{ print $2 }' | sed 's/[", ]//g')
 
-if [ "oldversion" = "version" ]; then echo "version not changed, exiting..." && exit 0; fi;
+if [ "$oldversion" = "$version" ]; then echo "version not changed, exiting..." && exit 0; fi;
 
 npx nswag run nswag.json && \
 rm openapi.yaml && \
