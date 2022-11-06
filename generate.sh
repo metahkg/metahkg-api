c36e28189bb83681d42e34b8cda47200ad28c3c4
wcyat <wcyat@wcyat.me>
2022-11-06 16:20:47 +0800

    fix generate.sh

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 wget "https://gitlab.com/metahkg/metahkg-server/-/raw/$(git symbolic-ref --short HEAD)/openapi.yaml"
-version=$(cat openapi.yaml | grep version | awk -F: '{ print $2 }' | sed 's/[" ]//g' | sed 's/-dev//g')
+version=$(cat openapi.yaml | grep version | awk -F: '{ print $2 }' | tr '\n' ' ' | sed 's/[" ]//g' | sed 's/-dev//g')
 oldversion=$(cat package.json | grep version | awk -F: '{ print $2 }' | sed 's/[", ]//g')
 
 # if [ "$oldversion" = "$version" ]; then echo "version not changed, exiting..." && rm openapi.yaml && exit 0; fi;
